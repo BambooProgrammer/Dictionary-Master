9af6c6421fc395484eff812435a9f61082dbb4a6
BambooProgeammer <prajwal.vs2010@outlook.com>
2024-09-18 17:05:11 +0530

    first commit

--- Dictionary_app.py
+++ Dictionary_app.py
@@ -12,25 +12,31 @@ def get_definition(word):
             meanings = data[0]['meanings']
             definitions = []
             for meaning in meanings:
-                definitions.append(f"• Meaning: {meaning['partOfSpeech']}\nDefinition: {meaning['definitions'][0]['definition']}\n")
+                   part_of_speech = meaning['partOfSpeech']
+                   for definition in meaning['definitions']:
+                          def_text = definition.get('definition', 'No definition found.')
+                          synonyms = definition.get('synonyms', [])
+                          antonyms = definition.get('antonyms', [])
+                          synonyms_text = ', '.join(synonyms) if synonyms else 'No synonyms found.'
+                          antonyms_text = ', '.join(antonyms) if antonyms else 'No antonyms found.'
+                          definitions.append(f"• Meaning: {part_of_speech}\n  Definition: {def_text}\n  Synonyms: {synonyms_text}\n Antonyms: {antonyms_text}\n")
             return '\n'.join(definitions)
         return "No Defintion found."
 
-def search_definition():
+def search_definition(event=None):
     word = entry_word.get()
     defintion = get_definition(word)
     text_output.configure(state=NORMAL)
     text_output.delete('1.0', 'end')
     text_output.insert('end', defintion)
-    text_output.configure(state=DISABLED, width=550, height=600)
-
+    text_output.configure(state=DISABLED, width=650, height=600)
 
 
 root = CTk()
 style = Style("flatly")
 root.title("Dictionary app")
 
-root.geometry("600x300")
+root.geometry("700x400")
 
 frame_search = CTkFrame(root)
 frame_search.pack(padx=20, pady=20)
@@ -40,13 +46,14 @@ label_word.grid(row=0, column=0, padx=5, pady=5)
 
 entry_word = CTkEntry(frame_search, width=200, font=("TkDefaultFont", 15))
 entry_word.grid(row=0, column=1, padx=5, pady=5)
+entry_word.bind('<Return>', search_definition)
 
 button_search = CTkButton(frame_search, text='Search', command=search_definition)
 button_search.grid(row=0, column=2
                    , padx=5, pady=5)
 
 frame_output = CTkFrame(root)
-frame_output.pack(padx=20, pady=10)
+frame_output.pack(padx=10, pady=10)
 
 text_output = CTkTextbox(frame_output, height=10, state=DISABLED, font=("TkDefaultFont", 15))
 text_output.pack()
